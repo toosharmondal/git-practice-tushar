78173cce10f8876bcc2df194023dd8ca4e1ac55d
Tushar Mondal <tooshar53@gmail.com>
2026-05-01 21:18:01 +0600

    Add basic calculator functionslator functions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
 from datetime import date
+from utils import add, subtract
 
 print("Tushar Mondal")
 
 print(date.today())
+
+sum = add(10,5)
+
+sub = subtract(100,77)
+
+print(f"Addition: {sum}")
+
+print(f"Subtraction: {sub}")
+
