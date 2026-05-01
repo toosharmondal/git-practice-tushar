3abbb7eee0dd294d4699178fb0611226f7ad3537
Tushar Mondal <tooshar53@gmail.com>
2026-05-01 21:55:41 +0600

    Added multiplication

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Tushar Mondal")
 
@@ -9,7 +9,11 @@ sum = add(10,5)
 
 sub = subtract(100,77)
 
+mul = multiply(21,11)
+
 print(f"Addition: {sum}")
 
 print(f"Subtraction: {sub}")
+
+print(f"Multiplication: {mul}")
 
